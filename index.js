6bd2fd6d87ce05d4a4fda39a12a31101aa6526a8
lmintsys <lubmin23@gmail.com>
2023-03-19 22:31:49 -0400

    added functions to view tables

--- index.js
+++ index.js
@@ -3,3 +3,22 @@ const cTable = require("console.table");
 const EmployeesDb = require("./lib/EmployeesDb");
 
 async function viewDepartments() {
+  const [rows] = await new EmployeesDb().getDepartments();
+
+  console.table(rows);
+  mainMenu();
+}
+
+async function viewRoles() {
+  const [rows] = await new EmployeesDb().getRoles();
+
+  console.table(rows);
+  mainMenu();
+}
+
+async function viewEmployees() {
+  const [rows] = await new EmployeesDb().getEmployees();
+
+  console.table(rows);
+  mainMenu();
+}
