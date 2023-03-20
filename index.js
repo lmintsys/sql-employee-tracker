a7759c88ac66545dbe863a420553aeade30cd9dd
lmintsys <lubmin23@gmail.com>
2023-03-19 22:41:14 -0400

    added functions to add to each table

--- index.js
+++ index.js
@@ -23,6 +23,72 @@ async function viewEmployees() {
   mainMenu();
 }
 
+async function addDepartment() {
+  inquirer.prompt(addDepartmentQuestions).then(async (answers) => {
+    await new EmployeesDb().addDepartment(answers.name);
+
+    console.log(`The department ${answers.name} was added`);
+    mainMenu();
+  });
+}
+
+async function addRole() {
+  const employeesDb = new EmployeesDb();
+
+  const [departmentRows] = await employeesDb.getDepartments();
+
+  const departmentChoices = departmentRows.map((departmentRow) => ({
+    value: departmentRow.id,
+    name: departmentRow.name,
+  }));
+
+  const addRoleQuestions = getAddRoleQuestions(departmentChoices);
+
+  inquirer.prompt(addRoleQuestions).then(async (answers) => {
+    await employeesDb.addRole(answers.name, answers.salary, answers.department);
+    console.log(answers.department);
+    console.log(`The role ${answers.name} was added`);
+    mainMenu();
+  });
+}
+
+async function addEmployee() {
+  const employeesDb = new EmployeesDb();
+
+  const [roleRows] = await employeesDb.getRoles();
+
+  const roleChoices = roleRows.map((roleRow) => ({
+    value: roleRow.id,
+    name: roleRow.title,
+  }));
+
+  const [managerRows] = await employeesDb.getEmployees();
+
+  const managerChoices = managerRows.map((managerRow) => ({
+    value: managerRow.id,
+    name: managerRow.first_name + " " + managerRow.last_name,
+  }));
+
+  const addEmployeeQuestions = getAddEmployeeQuestions(roleChoices, [
+    { value: null, name: "None" },
+    ...managerChoices,
+  ]);
+
+  inquirer.prompt(addEmployeeQuestions).then(async (answers) => {
+    await employeesDb.addEmployee(
+      answers.first_name,
+      answers.last_name,
+      answers.role,
+      answers.manager
+    );
+
+    console.log(
+      `The employee ${answers.first_name} ${answers.last_name} was added`
+    );
+    mainMenu();
+  });
+}
+
 const mainMenuQuestions = [
   {
     type: "list",
@@ -42,6 +108,58 @@ const mainMenuQuestions = [
       "Delete an employee",
       "Exit",
     ],
+  },
+];
+
+const addDepartmentQuestions = [
+  {
+    type: "input",
+    message: "What is the name of the department?",
+    name: "name",
+  },
+];
+
+const getAddRoleQuestions = (departmentChoices) => [
+  {
+    type: "input",
+    message: "What is the name of the role?",
+    name: "name",
+  },
+  {
+    type: "input",
+    message: "What is the salary of the role?",
+    name: "salary",
+  },
+  {
+    type: "list",
+    message: "Which department does the role belong to?",
+    name: "department",
+    choices: departmentChoices,
+  },
+];
+
+const getAddEmployeeQuestions = (roleChoices, managerChoices) => [
+  {
+    type: "input",
+    message: "What is the employee's first name?",
+    name: "first_name",
+  },
+  {
+    type: "input",
+    message: "What is the employee's last name?",
+    name: "last_name",
+  },
+  {
+    type: "list",
+    message: "What is the employee's role?",
+    name: "role",
+    choices: roleChoices,
+  },
+  {
+    type: "list",
+    message: "Who is the employee's manager?",
+    name: "manager",
+    choices: managerChoices,
   },
 ];
 
